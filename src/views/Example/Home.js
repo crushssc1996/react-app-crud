03b386a27162d63d43c484d59deb75ab890a06b9
SangTran <sang.tran@adurolife.com>
2021-12-22 16:44:12 +0700

    add redux

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from "react-router";
 import Color from '../HOC/Color';
 import logo from '../../assets/images/favicon.png';
-
+import { connect } from 'react-redux';
 class Home extends React.Component {
 
   componentDidMount() {
@@ -11,17 +11,51 @@ class Home extends React.Component {
     }, 3000)
   }
 
+  onHandleDeleteUser(user) {
+    this.props.onDeleteUser(user);
+  }
+
+  onHandleAddUser() {
+    this.props.onAddUser();
+  }
+  
   render() {
+    console.log('>> props: ', this.props.dataRedux)
+    let users = this.props.dataRedux;
     return (
       <>
-        <div>Home page - Hello Reactjs</div>
-        <did>
-          <img alt="logo" src={logo} style={{ width: '200px', height: '200px', marginTop: '20px'}}/>
-        </did>
+        <div>HOME PAGE - Hello Reactjs</div>
+        <img alt="logo" src={logo} style={{ width: '20px', height: '20px', marginTop: '20px'}}/>
+        {users && users.length > 0 &&
+          users.map((item, idx) => {
+            return (
+              <div key={item.id}>
+                {idx + 1} - { item.name } ------ 
+                <span onClick={() => this.onHandleDeleteUser(item)}>X</span>
+              </div>
+            )
+          })
+        }
+        <div>
+          <button onClick={() => this.onHandleAddUser()}>Add New</button>
+        </div>
       </>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    dataRedux: state.users
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDeleteUser: (user) => dispatch({ type: 'DELETE_USER', user}),
+    onAddUser: () => dispatch({ type: 'ADD_USER'})
+  }
+}
+
 // export default withRouter(Home);
-export default Color(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
